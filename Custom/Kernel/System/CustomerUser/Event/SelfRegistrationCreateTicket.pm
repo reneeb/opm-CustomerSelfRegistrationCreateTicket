2f7156a0694799d25bed4b7600384d9e320c6824
reneeb <reb@perl-services.de>
2016-02-11 12:57:24 +0100

    initial commit

---
+++ Custom/Kernel/System/CustomerUser/Event/SelfRegistrationCreateTicket.pm
@@ -0,0 +1,129 @@
+# --
+# Copyright (C) 2016 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::CustomerUser::Event::SelfRegistrationCreateTicket;
+
+use strict;
+use warnings;
+
+use Kernel::System::ObjectManager;
+
+our @ObjectDependencies = qw(
+    Kernel::Config
+    Kernel::Language
+    Kernel::System::Log
+    Kernel::System::Ticket
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
+    my $LanguageObject = $Kernel::OM->Get('Kernel::Language');
+    my $LogObject      = $Kernel::OM->Get('Kernel::System::Log');
+    my $TicketObject   = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    # create ticket only when customer user registered itself
+    my $IsSelfRegistered;
+
+    LEVEL:
+    for my $Level ( 1 .. 4 ) {
+        my ($Package) = caller( $Level );
+        if ( $Package eq 'Kernel::System::Web::InterfaceCustomer' ) {
+            $IsSelfRegistered = 1;
+            last LEVEL;
+        }
+    }
+
+    return 1 if !$IsSelfRegistered;
+
+    # check needed stuff
+    for my $Needed (qw( Data Event Config UserID )) {
+        if ( !$Param{$Needed} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!"
+            );
+
+            return;
+        }
+    }
+
+    for my $NeededData (qw( UserLogin NewData )) {
+        if ( !$Param{Data}->{$NeededData} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $NeededData in Data!"
+            );
+
+            return;
+        }
+    }
+
+    my $Title = $LanguageObject->Translate(
+        $ConfigObject->Get('CUSRTC::Title') // 'CustomerUser Created',
+    );
+
+    my $Queue    = $ConfigObject->Get('CUSRTC::Queue')    // 'Misc';
+    my $Priority = $ConfigObject->Get('CUSRTC::Priority') // '3 normal';
+    my $State    = $ConfigObject->Get('CUSRTC::State')    // 'new';
+
+    my $TicketID = $TicketObject->TicketCreate(
+        Title        => $Title,
+        Queue        => $Queue,
+        Lock         => 'unlock',
+        Priority     => $Priority,
+        State        => $State,
+        CustomerID   => $Param{Data}->{NewData}->{UserCustomerID},
+        CustomerUser => $Param{Data}->{NewData}->{UserLogin},
+        OwnerID      => 1,
+        UserID       => 1,
+    );
+
+    return 1 if !$TicketID;
+
+    my $LocalOM      = Kernel::System::ObjectManager->new();
+    my $LayoutObject = $LocalOM->Get('Kernel::Output::HTML::Layout');
+
+    my $Body = $LayoutObject->Output(
+        Template => ( $ConfigObject->Get('CUSRTC::Body') // 'New User: [% Data.UserLogin %]' ),
+        Data     => $Param{Data}->{NewData},
+    );
+
+    $Body = $LayoutObject->RichTextDocumentComplete(
+        String => $Body,
+    );
+
+    $TicketObject->ArticleCreate(
+        TicketID       => $TicketID,
+        ArticleType    => 'note-internal',
+        SenderType     => 'system',
+        From           => $Param{Data}->{NewData}->{UserEmail},
+        To             => 'otrs@localhost',
+        Subject        => $Title,
+        Body           => $Body,
+        ContentType    => 'text/html; charset=utf-8',
+        HistoryType    => 'AddNote',
+        HistoryComment => '%%',
+        UserID         => 1,
+    );
+
+    return 1;
+}
+
+1;
